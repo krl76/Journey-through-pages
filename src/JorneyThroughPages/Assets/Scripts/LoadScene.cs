9464744bfb5e59fcb0c9ac2b7c3a89b6de8ff1db
krl76 <k.kornilov2005@yandex.ru>
2024-04-27 00:50:20 +0300

    base ui done

--- src/JorneyThroughPages/Assets/Scripts/LoadScene.cs
+++ src/JorneyThroughPages/Assets/Scripts/LoadScene.cs
@@ -1,30 +1,51 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 using Slider = UnityEngine.UI.Slider;
 
 public class LoadScene : MonoBehaviour
 {
-    [SerializeField] private GameObject _canvas;
+    [SerializeField] private Animator _loadingScreen;
     [SerializeField] private Slider _progressSlider;
+    [SerializeField] private bool _endAnim;
     
     private float progressValue;
-
+    
+    public void EndLoadingAnimation()    { _endAnim = false; }
+    
+    //public void Awake() { DontDestroyOnLoad(this); }
+    
     public void SceneLoad(string nameScene)
     {
-        _canvas.SetActive(true);
+        Time.timeScale = 1f;
+        _loadingScreen.SetTrigger("StartAnim");
+        _endAnim = true;
+        _progressSlider.value = 0;
         StartCoroutine(LoadSceneAsync(nameScene));
     }
 
     private IEnumerator LoadSceneAsync(string nameScene)
     {
-        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(nameScene);
-
-        while (!loadOperation.isDone)
+        while (_endAnim)
         {
-            progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
-            _progressSlider.value = progressValue;
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        AsyncOperation loading = SceneManager.LoadSceneAsync(nameScene);
+        loading.allowSceneActivation = false;
+
+        while (loading.progress < 0.9f)
+        {
+            _progressSlider.value = _progressSlider.value = Mathf.Clamp01(loading.progress / 0.9f);
             yield return null;
         }
+
+        _progressSlider.value = _progressSlider.value = Mathf.Clamp01(loading.progress / 0.9f);
+        yield return new WaitForSeconds(0.1f);
+        loading.allowSceneActivation = true;
+        _loadingScreen.SetTrigger("EndAnim");
     }
 }
