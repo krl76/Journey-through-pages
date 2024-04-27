e250e0d3471223db380a245dd9d732ce67882dac
artem m. <105444505+havaise@users.noreply.github.com>
2024-04-28 01:47:19 +0300

    123

---
+++ src/JorneyThroughPages/Assets/Scripts/RoseScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoseScript : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up, _rotationSpeed * Time.deltaTime);
+    }
+}
