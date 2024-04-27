efc011c363f44fb4c1014e1c5bd13663a924a3db
krl76 <k.kornilov2005@yandex.ru>
2024-04-27 03:25:53 +0300

    rework dialog system

--- src/JorneyThroughPages/Assets/DialogKing.cs
+++ src/JorneyThroughPages/Assets/DialogKing.cs
@@ -3,55 +3,162 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.UI;
 
 public class DialogKing : MonoBehaviour
 {
-    public TextMeshProUGUI textComponent;
-    public string[] lines;
-    public float textSpeed;
-    private int index;
+    [Header("Text Settings")]
+    [SerializeField] private TextMeshProUGUI textComponent;
+    [SerializeField] private float textSpeed;
+    
+    [Header("King Dialogs")]
+    [SerializeField] private string[] king1;
+    [SerializeField] private string[] king2;
+    
+    [Header("Honor Dialogs")]
+    [SerializeField] private string[] honor1;
+    [SerializeField] private string[] honor2;
+    
+    [Header("Drunk Dialogs")]
+    [SerializeField] private string[] drunk1;
+    [SerializeField] private string[] drunk2;
+    
+    [Header("Light Dialogs")]
+    [SerializeField] private string[] light1;
+    [SerializeField] private string[] light2;
+    
+    [Header("Deal Dialogs")]
+    [SerializeField] private string[] deal1;
+    [SerializeField] private string[] deal2;
+    
+    [Header("Geo Dialogs")]
+    [SerializeField] private string[] geo1;
+    [SerializeField] private string[] geo2;
 
-    void Start()
+    
+    private int index;
+    
+    private PlayerAction playerAction;
+    private string[] lineNow;
+
+    private void Awake()
     {
-        textComponent.text = string.Empty;
-        StartDialog();
+        playerAction = new PlayerAction();
+
+        playerAction.Dialogs.ScrollDialog.started += ctx => ScrollDia();
     }
 
-    void Update()
+    private void OnEnable()
     {
-        if(Input.GetMouseButtonDown(0))
+        playerAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerAction.Disable();
+    }
+
+    private void ScrollDia()
+    {
+        if(textComponent.text == lineNow[index])
+            NextLine(lineNow);
+        else
         {
-            if(textComponent.text == lines[index])
-                NextLine();
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
+            StopAllCoroutines();
+            textComponent.text = lineNow[index];
         }
     }
 
-    void StartDialog()
+    /*void Start()
     {
+        textComponent.text = string.Empty;
+        StartDialog();
+    }*/
+
+    public void StartDialog(string name, int iter)
+    {
+        switch (name)
+        {
+            case "King":
+                if (iter == 1)
+                {
+                    StartCoroutine(TypeLine(king2));
+                }
+                if (iter == 0)
+                {
+                    StartCoroutine(TypeLine(king1));
+                }
+                break;
+            case "Honor":
+                if (iter == 1)
+                {
+                    StartCoroutine(TypeLine(honor2));
+                }
+                if (iter == 0)
+                {
+                    StartCoroutine(TypeLine(honor1));
+                }
+                break;
+            case "Drunk":
+                if (iter == 1)
+                {
+                    StartCoroutine(TypeLine(drunk2));
+                }
+                if (iter == 0)
+                {
+                    StartCoroutine(TypeLine(drunk1));
+                }
+                break;
+            case "Light":
+                if (iter == 1)
+                {
+                    StartCoroutine(TypeLine(light2));
+                }
+                if (iter == 0)
+                {
+                    StartCoroutine(TypeLine(light1));
+                }
+                break;
+            case "Deal":
+                if (iter == 1)
+                {
+                    StartCoroutine(TypeLine(deal2));
+                }
+                if (iter == 0)
+                {
+                    StartCoroutine(TypeLine(deal1));
+                }
+                break;
+            case "Geo":
+                if (iter == 1)
+                {
+                    StartCoroutine(TypeLine(geo2));
+                }
+                if (iter == 0)
+                {
+                    StartCoroutine(TypeLine(geo1));
+                }
+                break;
+        }
         index = 0;
-        StartCoroutine(TypeLine());
     }
 
-    IEnumerator TypeLine()
+    IEnumerator TypeLine(string[] line)
     {
-        foreach(char c in lines[index].ToCharArray())
+        lineNow = line;
+        foreach(char c in line[index].ToCharArray())
         {
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
     }
-    void NextLine()
+    void NextLine(string[] line)
     {
-        if (index < lines.Length - 1)
+        if (index < line.Length - 1)
         {
             index++;
             textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
+            StartCoroutine(TypeLine(king1));
         }
         else
         {
